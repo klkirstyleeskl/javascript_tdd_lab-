239e469b9628b0228de8a073f57beef6d43f8bb9
klkirstyleeskl <k.lees@btinternet.com>
2019-01-08 16:23:08 +0000

    adds start of add paint to stock function

--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,14 +1,20 @@
 const assert = require('assert');
 const Decorator = require('../decorator');
+const PaintCan = require('../paint_can')
 
 describe('Decorator', function() {
   let decorator;
   beforeEach(function() {
     decorator = new Decorator([])
+    paint_can = new PaintCan(100)
   });
+  
   it('should have stock', function() {
     const actual = decorator.stock;
     assert.deepStrictEqual(actual, [ ]);
   });
+  it('should add paint to stock', function() {
+
+  })
 
 });
